f289e6a3564b68df8ada2f9f76fa097fe5ff478f
Sai Mahajan <160035712+SaaiM2005@users.noreply.github.com>
2024-10-15 19:24:13 +0530

    Add files via upload

---
+++ JFrm20.java
@@ -0,0 +1,39 @@
+import java.awt.*; 
+import java.awt.event.*; 
+import javax.swing.*; 
+import javax.swing.event.ChangeEvent; 
+import javax.swing.event.ChangeListener;
+class JFrm20 extends JFrame
+{
+    JProgressBar p1;
+    int i=0;
+    JFrm20()
+    {
+        super("Progress Bar");
+        p1=new JProgressBar(0,10000);
+        p1.setBounds(40,40,160,30);
+        p1.setValue(0);
+        p1.setStringPainted(true);
+        add(p1);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public void iterate()
+    {
+        while(i<=10000)
+        {
+            p1.setValue(i);
+            i+=20; //INCREMENT BY 20
+            try
+            {
+                Thread.sleep(100);
+            }catch(Exception e){}
+        }
+    }
+    public static void main(String[] args) 
+    {
+        JFrm20 a=new JFrm20();
+        a.setVisible(true);
+        a.iterate();
+    }
+}
